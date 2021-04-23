10e84aee83e29897d7f8453bdb5e4163ce855468
dgomesreal <dgomesreal@gmail.com>
2021-04-23 12:02:49 -0300

    Primeiro Commit

---
+++ CorePDFReport/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using CorePDFReport.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wkhtmltopdf.NetCore;
+
+namespace CorePDFReport.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        readonly IGeneratePdf generatePdf;
+
+        public HomeController(IGeneratePdf generatePdf)
+        {
+            this.generatePdf = generatePdf;
+        }
+        
+        [HttpGet]
+        [Route("Get")]
+        public async Task<ActionResult> GetEmployeeInfo()
+        {
+            var empObj = new EmployeeInfo
+            {
+                EmpId = "0101",
+                EmpName = "Carlos",
+                Department = "IT",
+                Designation = "Software Engineer"
+            };
+
+            return (ActionResult)await this.generatePdf.GetPdf("Views/Employee/EmployeeInfo.cshtml", empObj);
+        }
+    }
+}
